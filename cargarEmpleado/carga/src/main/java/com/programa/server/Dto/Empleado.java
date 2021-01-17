153ee47596c631305cccd9c157d5134475991cbe
afsolanoc95 <afsolanoc95@gmail.com>
2021-01-17 12:55:01 -0500

    Update Empleado.java

--- cargarEmpleado/carga/src/main/java/com/programa/server/Dto/Empleado.java
+++ cargarEmpleado/carga/src/main/java/com/programa/server/Dto/Empleado.java
@@ -43,39 +43,98 @@ public class Empleado implements Serializable{
 	}
 
 	public void setFechaNacimiento(String fechaNacimiento) {
-		List<String> formatStrings = Arrays.asList("yyyyMMdd");
-		for (String formatString : formatStrings)
-	    { 
 			try {
-			        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(formatString);
-			        Date date = simpleDateFormat.parse(fechaNacimiento);
-			        this.fechaNacimiento = date;
+			        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+			        
+			        if(isValidDate(fechaNacimiento)) {
+			        	Date date = simpleDateFormat.parse(fechaNacimiento);
+				        this.fechaNacimiento = date;
+			        }else {
+			        	this.fechaNacimiento = null;
+			        	System.out.println("formato fecha incorrecta ");
+			        }
+			        
 			} 
 			        catch (Exception e) {
 			        	this.fechaNacimiento = null;
 			        	System.out.println("formato fecha incorrecta "+e.getMessage());
 			      }
-	    }
+
 	}
 
+	 public static boolean isValidDate(String dateString) {
+	       if (dateString == null || dateString.length() != "yyyyMMdd".length()) {
+	           return false;
+	       }
+
+	       int date;
+	       try {
+	           date = Integer.parseInt(dateString);
+	       } catch (NumberFormatException e) {
+	           return false;
+	       }
+
+	       int year = date / 10000;
+	       int month = (date % 10000) / 100;
+	       int day = date % 100;
+
+	       // leap years calculation not valid before 1581
+	       boolean yearOk = (year >= 1581) && (year <= 2500);
+	       boolean monthOk = (month >= 1) && (month <= 12);
+	       boolean dayOk = (day >= 1) && (day <= daysInMonth(year, month));
+
+	       return (yearOk && monthOk && dayOk);
+	}
+
+	private static int daysInMonth(int year, int month) {
+	       int daysInMonth;
+	       switch (month) {
+	           case 1: 
+	           case 3: 
+	           case 5: 
+	           case 7: 
+	           case 8: 
+	           case 10: 
+	           case 12:
+	               daysInMonth = 31;
+	               break;
+	           case 2:
+	               if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+	                   daysInMonth = 29;
+	               } else {
+	                   daysInMonth = 28;
+	               }
+	               break;
+	           default:
+	               daysInMonth = 30;
+	       }
+	       return daysInMonth;
+	}
+	
 	public Date getFechaVinculacion() {
 		return fechaVinculacion;
 	}
 
 	public void setFechaVinculacionEmpresa(String fechaVinculacion) {
-		List<String> formatStrings = Arrays.asList("yyyyMMdd");
-		for (String formatString : formatStrings)
-	    { 
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+
 			try {
-			        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(formatString);
-			        Date date = simpleDateFormat.parse(fechaVinculacion);
-			        this.fechaVinculacion = date;
+				
+				
+				 if(isValidDate(fechaVinculacion)) {
+			        	Date date = simpleDateFormat.parse(fechaVinculacion);
+				        this.fechaVinculacion = date;
+			        }else {
+			        	this.fechaVinculacion = null;
+			        	System.out.println("formato fecha incorrecta ");
+			        }
+
 			} 
-			        catch (Exception e) {
+			catch (Exception e) {
 			        	this.fechaVinculacion = null;
 			        	System.out.println("formato fecha incorrecta "+e.getMessage());
-			      }
-	    }
+			}
+	    
 	}
 
 	public Double getSalario() {
